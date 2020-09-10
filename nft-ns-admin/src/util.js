de7bfb25748f5e449dd05a9d70adfc3a5a251225
zhware <zh@zhware.net>
2020-09-10 20:55:51 +0900

    duplicate method removed

--- nft-ns-admin/src/util.js
+++ nft-ns-admin/src/util.js
@@ -529,42 +529,6 @@ class Util {
       throw error
     }
   }
-
-  // Get biggest UTXO
-  // Returns the utxo with the biggest balance from an array of utxos.
-  async findBiggestUtxo (utxos) {
-    try {
-      let largestAmount = 0
-      let largestIndex = 0
-
-      for (var i = 0; i < utxos.length; i++) {
-        const thisUtxo = utxos[i]
-        // console.log(`thisUTXO: ${JSON.stringify(thisUtxo, null, 2)}`);
-
-        // Validate the UTXO data with the full node.
-        /* eslint-disable no-await-in-loop */
-        const txout = await bchjs.Blockchain.getTxOut(thisUtxo.tx_hash, thisUtxo.tx_pos)
-        /* eslint-enable no-await-in-loop */
-        if (txout === null) {
-          // If the UTXO has already been spent, the full node will respond with null.
-          console.log(
-            'Stale UTXO found. You may need to wait for the indexer to catch up.'
-          )
-          continue
-        }
-
-        if (thisUtxo.value > largestAmount) {
-          largestAmount = thisUtxo.value
-          largestIndex = i
-        }
-      }
-
-      return utxos[largestIndex]
-    } catch (error) {
-      console.error('Error in findBiggestUtxo: ', error)
-      throw error
-    }
-  }
 }
 
 module.exports = Util
